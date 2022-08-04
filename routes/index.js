afbfdf13be000138fd1a4b5d733f8932201e7c65
Remiferiaa <slong819@hotmail.com>
2022-08-04 21:21:04 +0800

    temporary layout done and route file placeholders

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Arknights Material Compendium' });
 });
 
 module.exports = router;
