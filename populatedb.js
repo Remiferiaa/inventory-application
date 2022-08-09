b7b72dc6b6a7046f772911a6d01b18f0a926185f
Remiferiaa <slong819@hotmail.com>
2022-08-09 15:44:34 +0800

    stage name update

--- populatedb.js
+++ populatedb.js
@@ -96,13 +96,13 @@ const newContributor = function(cb) {
 const newStage = function(cb) {
   async.series([
     function(callback) {
-      createStage('LS-5', "Annihilation Drill\nDefend against the enemy's surprise attack.\nDeployment Points will not automatically recover in this operation; You get 1 Deployment Point upon killing 1 enemy.", 30, 'https://prts.wiki/images/thumb/c/c5/LS-5_%E6%AD%BC%E7%81%AD%E6%88%98%E6%BC%94%E4%B9%A0_%E5%9C%B0%E5%9B%BE.png//600px-LS-5_%E6%AD%BC%E7%81%AD%E6%88%98%E6%BC%94%E4%B9%A0_%E5%9C%B0%E5%9B%BE.png', contributors[0], callback)
+      createStage('LS-5, Annihilation Drill', "Defend against the enemy's surprise attack.\nDeployment Points will not automatically recover in this operation; You get 1 Deployment Point upon killing 1 enemy.", 30, 'https://prts.wiki/images/thumb/c/c5/LS-5_%E6%AD%BC%E7%81%AD%E6%88%98%E6%BC%94%E4%B9%A0_%E5%9C%B0%E5%9B%BE.png//600px-LS-5_%E6%AD%BC%E7%81%AD%E6%88%98%E6%BC%94%E4%B9%A0_%E5%9C%B0%E5%9B%BE.png', contributors[0], callback)
     },
     function(callback) {
-      createStage('4-6', 'Ignorance\nUnknown creatures infected with Oripathy have been found on the battlefield. They deal massive damage to nearby Operators when they die.\nIn addition, please look out for Heated Paths. They emit burning gas.\n[Heated Path] Deals massive damage to Operators and enemies on it at intervals.',18, 'https://prts.wiki/images/thumb/3/3b/4-6_%E5%B0%91%E8%A7%81%E5%A4%9A%E6%80%AA_%E5%9C%B0%E5%9B%BE.png/600px-4-6_%E5%B0%91%E8%A7%81%E5%A4%9A%E6%80%AA_%E5%9C%B0%E5%9B%BE.png', contributors[0], callback)
+      createStage('4-6, Ignorance', 'Unknown creatures infected with Oripathy have been found on the battlefield. They deal massive damage to nearby Operators when they die.\nIn addition, please look out for Heated Paths. They emit burning gas.\n[Heated Path] Deals massive damage to Operators and enemies on it at intervals.',18, 'https://prts.wiki/images/thumb/3/3b/4-6_%E5%B0%91%E8%A7%81%E5%A4%9A%E6%80%AA_%E5%9C%B0%E5%9B%BE.png/600px-4-6_%E5%B0%91%E8%A7%81%E5%A4%9A%E6%80%AA_%E5%9C%B0%E5%9B%BE.png', contributors[0], callback)
     },
     function(callback) {
-      createStage('7-18', "Death of a Patriot\nThey are commanded by the 'Aegis' of the Infected. The most ancient bloodline among the warriors of Sarkaz. Ursus's indomitable incarnation of war. The backbone of the Reunion Movement. Yelena's father.\n[Originium Altar] Periodically emits Pulse Waves to the surrounding tiles, damaging both allied and enemy units.\n[Interference Mine]Upon explosion, deals heavy damage to enemies in the surrounding tiles and amplifies the damage they take for 15 seconds.", 21, false, contributors[1], callback )
+      createStage('7-18, Death of a Patriot', "They are commanded by the 'Aegis' of the Infected. The most ancient bloodline among the warriors of Sarkaz. Ursus's indomitable incarnation of war. The backbone of the Reunion Movement. Yelena's father.\n[Originium Altar] Periodically emits Pulse Waves to the surrounding tiles, damaging both allied and enemy units.\n[Interference Mine]Upon explosion, deals heavy damage to enemies in the surrounding tiles and amplifies the damage they take for 15 seconds.", 21, false, contributors[1], callback )
     }
   ], cb)
 }
